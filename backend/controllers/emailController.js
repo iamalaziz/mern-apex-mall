b5924a880b1879a73d962597891d048da6a6ca93
iamalaziz <mashrabovabdulaziz2463@gmail.com>
2023-05-27 17:23:17 +0900

    changed response in sendEmail

--- backend/controllers/emailController.js
+++ backend/controllers/emailController.js
@@ -10,6 +10,7 @@ const oAuth2Client = new google.auth.OAuth2(
   process.env.CLIENT_SECRET,
   process.env.REDIRECT_URI
 );
+
 oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
 
 const sendOTPVerificationEmail = async ({ _id, email }, res) => {
@@ -38,6 +39,7 @@ const sendOTPVerificationEmail = async ({ _id, email }, res) => {
         accessToken: accessToken,
       },
     });
+
     await new Promise((resolve, reject) => {
       transporter.verify((error, success) => {
         if (error) {
@@ -60,19 +62,16 @@ const sendOTPVerificationEmail = async ({ _id, email }, res) => {
     };
     await transporter.sendMail(mailOptions);
 
-    return {
+    res.json({
       status: 'Pending...',
       message: 'Verification OTP email is sent!',
       data: {
         userId: _id,
         email,
       },
-    };
-  } catch (error) {
-    res.json({
-      status: 'Failed',
-      message: error.message,
     });
+  } catch (error) {
+    console.log(error.message)
   }
 };
 
